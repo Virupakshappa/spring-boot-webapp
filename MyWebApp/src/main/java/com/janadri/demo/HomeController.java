45481f887476e312467b4a96b8884c90c1961221
Veeru <vjanadri7@gmail.com>
2022-02-10 13:53:49 +0530

    Jp-22: Updating the branch with controller implementation

---
+++ MyWebApp/src/main/java/com/janadri/demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.janadri.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("home")    //calls this method whenever client hits /home endpoint
+//	@ResponseBody              //used to return data in string format(treats "home.jsp" as a string)
+	public String homeMethod() {
+		System.out.println("Hey I am in home method");
+		
+		return "home.jsp";
+	}
+}
